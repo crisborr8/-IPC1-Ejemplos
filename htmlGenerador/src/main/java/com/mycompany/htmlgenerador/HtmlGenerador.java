75bbfe00f08861da6c8ceb63bca47b4aea40160c
Christofer Borrayo <34244533+crisborr8@users.noreply.github.com>
2023-02-23 17:44:47 -0600

    feat: Added html generador para reportes

---
+++ htmlGenerador/src/main/java/com/mycompany/htmlgenerador/HtmlGenerador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.htmlgenerador;
+
+/**
+ *
+ * @author criss
+ */
+public class HtmlGenerador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ htmlGenerador/src/main/java/com/mycompany/htmlgenerador/Usuario.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.htmlgenerador;
+
+/**
+ *
+ * @author criss
+ */
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String tarjeta;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTarjeta() {
+        return tarjeta;
+    }
+
+    public void setTarjeta(String tarjeta) {
+        this.tarjeta = tarjeta;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
